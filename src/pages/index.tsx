2b0ef17975a7b8d34660fae43f26e8d18aa45d36
EricNguyenFly <huynguyen.flyteam@gmail.com>
2023-09-15 15:19:41 +0700

    update payment token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ const entryPoint = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 const simpleAccountFactory = "0x9406Cc6185a346906296840746125a0E44976454";
 const paymentToken = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B";
 const pmContext = {
+  // pay-as-you-go Stackup's will pay gas
   // type: "payg",
   type: "erc20token",
   token: paymentToken
@@ -137,7 +138,7 @@ export default function Home() {
       const res = await client.sendUserOperation(
         account.execute(token,
           0,
-          erc20.interface.encodeFunctionData("approve", [to, amount])),
+          erc20.interface.encodeFunctionData("approve", [to, value])),
         {
           onBuild: async (op) => {
             addEvent(`Signed UserOperation: `);
@@ -291,7 +292,7 @@ export default function Home() {
                       Transfer ERC20 {" "}
                     </h2>
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                      Simple transfer 1 amount of token ERC20.
+                      Simple transfer 0.0001 amount of token ERC20.
                     </p>
                   </button>
                 </div>
